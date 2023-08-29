346921f94a25f77a95360b8c2dd63c016bfed163
mikay568 <142894181+mikay568@users.noreply.github.com>
2023-08-29 07:54:22 -0300

    Add files via upload

---
+++ media numero.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    float A,B,X;
+    printf("escreva dois numeros para ver a media\n");
+    scanf("%f",&A);
+    scanf("%2f",&B);
+    X=(A+B)/2;
+    printf("Media:%3f\n",X);
+}
