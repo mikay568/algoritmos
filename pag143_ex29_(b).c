ad9ed2faffc02f4928369dc88ee788c06d5a0316
mikay568 <142894181+mikay568@users.noreply.github.com>
2023-09-19 07:52:35 -0300

    Add files via upload

---
+++ pag143_ex29_(b).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main ()
+{float N;
+printf("escreva um numero positivo ou negativo\n");
+    scanf("%f", &N);
+    if(N>0)
+   printf("numero positivo");
+  if(N<0)
+    printf("numero negativo");
+}
