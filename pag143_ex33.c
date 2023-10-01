de4d9f9b410c9bf1e2847330912bf68e7b573c23
mikay568 <142894181+mikay568@users.noreply.github.com>
2023-10-01 19:00:29 -0300

    Update pag143_ex33.c

--- pag143_ex33.c
+++ pag143_ex33.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
-#include<math.h>
+
 int main()
 {
     int N1,N2,R;
+    printf("escreva dois numeros para saber se suas somas sao unidade, dezena ou centena:");
     scanf("%i %i",&N1,&N2);
     R=N1+N2;
-    printf("%i",R);
-    if((0<R) && (R<9));
+    if((0<R)&&(R<9))
     {
-        printf("resultado:unidade\n",R);
-    }
-    if((9<=R) && (R<100));
+       printf("resultado:unidade\n",R);
+     }
+    if((9<=R)&&(R<100))
     {
-        printf("resultado:dezena\n",R);
-
-    }
-    if((100<=R) && (R<=999));
+       printf("resultado:dezena\n",R);
+     }
+    if((100<=R)&&(R<=999))
     {
-        printf("resultado:centena\n",R);
-    }
+       printf("resultado:centena\n",R);
+     }
 }
