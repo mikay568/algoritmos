9314caaab29df647373d7d8eafc4b8e337be7999
mikay568 <142894181+mikay568@users.noreply.github.com>
2023-10-03 08:39:40 -0300

    Add files via upload

---
+++ desmonstração notas.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+float main()
+{
+    float Md1, Md2, N1, N2, N3, N4, N5;
+    printf("escreva as 4 primeiras notas da media1:");
+    scanf("%f %f %f %f", &N1,&N2,&N3,&N4);
+    Md1 = (N1+N2+N3+N4)/4;
+    printf("media:%.2f\n",Md1);
+    if (Md1 >= 7)
+    {
+        printf("aprovado");
+    }
+    else  if(Md1<7)
+    {   printf("escreva a nota do exame:");
+        scanf("%f",&N5);
+        Md2= (Md1+N5)/2;
+    }
+      else   if(Md2 >= 5)
+           {
+            printf("aprovado por exame:%.2f",Md2);
+        }
+    else    if(Md2<5)
+        {
+            printf("reprovado:%.2f",Md2);
+        }
+    else      if((Md2<=1)&&(Md2>=4))
+     {
+         printf("I");
+         }
+    else    if((Md2<4)&&(Md2>=6))
+     {
+         printf("R");
+         }
+    else   if((Md2<6)&&(Md2>=8))
+      {
+          printf("B");
+          }
+    else   if((Md2<8)&&(Md2>=10))
+       {
+           printf("MB");
+           }
+     }
+
