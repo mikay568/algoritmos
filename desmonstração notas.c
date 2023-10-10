74184bd7ee555ee4491e01d83ae088410c108ee9
mikay568 <142894181+mikay568@users.noreply.github.com>
2023-10-10 08:27:59 -0300

    Update desmonstração notas.c

--- desmonstração notas.c
+++ desmonstração notas.c
@@ -2,21 +2,15 @@
 float main()
 {
     float Md1, Md2, N1, N2, N3, N4, N5;
+    printf("-------------------------------------\n");
     printf("escreva as 4 primeiras notas da media:");
     scanf("%f %f %f %f", &N1,&N2,&N3,&N4);
+    printf("--------------------------------------\n");
     Md1 = (N1+N2+N3+N4)/4;
     printf("media:%.2f\n",Md1);
     if (Md1 >= 7)
-    {
+    {printf("+======================+\n");
         printf("aprovado\nmencao:");
-          if((Md1>=1)&&(Md1<=4))
-        {
-         printf("I");
-        }
-        if((Md1>4)&&(Md1<=6))
-        {
-         printf("R");
-        }
       if((Md1>6)&&(Md1<=8))
       {
           printf("B");
@@ -25,35 +19,37 @@ float main()
        {
            printf("MB");
            }
-    }
+     printf("\n+=====================+\n"); }
     if(Md1<7)
-    {   printf("escreva a nota do exame:");
+    { printf("-------------------------------------\n");
+          printf("escreva a nota do exame:");
         scanf("%f",&N5);
+      printf("--------------------------------------\n");
         Md2= (Md1+N5)/2;
         if(Md2 >= 5)
-        {
-            printf("aprovado por exame:%.2f\nmencao:",Md2);
+       {
+        printf("+======================+\n");
+          printf("aprovado por exame:%.2f\nmencao:",Md2);
         }
-       if(Md2<5)
-        {
-            printf("reprovado:%.2f\nmencao:",Md2);
+        if(Md2<5)
+        {printf("+======================+\n");
+          printf("reprovado:%.2f\nmencao:",Md2);
         }
-         if((Md2>=1)&&(Md2<=4))
+        if((Md2>=0)&&(Md2<=4))
         {
-         printf("I");
-        }
+          printf("I");
+        printf("\n+======================+");}
         if((Md2>4)&&(Md2<=6))
         {
-         printf("R");
-        }
-      if((Md2>6)&&(Md2<=8))
+          printf("R");
+        printf("\n+======================+");}
+        if((Md2>6)&&(Md2<=8))
       {
           printf("B");
-          }
-       if((Md2>8)&&(Md2<=10))
+        printf("\n+======================+");}
+        if((Md2>8)&&(Md2<=10))
        {
-           printf("MB");
-           }
+          printf("MB");
+        printf("\n+======================+");}
      }
 }
-
